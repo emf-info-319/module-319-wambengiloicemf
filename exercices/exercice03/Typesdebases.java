f48c31378d1bb9a639ccfc4d38918181724343f1
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-08-29 11:40:54 +0200

    Exercice 03

---
+++ exercices/exercice03/Typesdebases.java
@@ -0,0 +1,14 @@
+package exercices.exercice03;
+
+public class Typesdebases {
+    public static void main(String[] args) { 
+        byte age =22;
+        String nom = "Loic";
+        System.out.println(nom+" "+age);
+
+    int agedans10ans = age+10;
+
+    System.out.println( "Et dans 10 ans j'aurais" + agedans10ans);
+    }
+
+}
