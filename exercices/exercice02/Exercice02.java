a8e476b8cb9351d4655c32477e90cb95923e3250
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-08-29 09:48:01 +0200

    exercice 02

---
+++ exercices/exercice02/Exercice02.java
@@ -0,0 +1,11 @@
+package exercices.exercice02;
+
+public class Exercice02 {
+    public static void main(String[] args) {
+        //commentaire ligne 1
+
+System.out.println("Bonjour tout le monde!");
+
+    }
+
+}
---
+++ exercices/exercice02/Exercice02_bis.java
@@ -0,0 +1,8 @@
+package exercices.exercice02;
+
+public class Exercice02_bis {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+    
+}
