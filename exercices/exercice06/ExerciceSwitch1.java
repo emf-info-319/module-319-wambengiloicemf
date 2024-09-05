db21d7e2299e27939e3f2f6bd40ae59d2130d24f
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-05 11:43:44 +0200

    jeudi

--- exercices/exercice06/ExerciceSwitch1.java
+++ exercices/exercice06/ExerciceSwitch1.java
@@ -2,7 +2,7 @@ package exercice06;
 
 public class ExerciceSwitch1 {
     public static void main(String[] args) {
-        int noteDuModule = 5;
+        int noteDuModule = 2;
 
         if (noteDuModule == 1) {
             System.out.println("Travail non rendu");
@@ -17,7 +17,30 @@ public class ExerciceSwitch1 {
         } else if(noteDuModule==6) {
             System.out.println("Très bien");
         }
-    
+    switch (noteDuModule) {
+        case 1:
+        System.out.println("Travail non rendu"); 
+            break;
+        case 2:
+        System.out.println("Très insuffisant");
+            break;
+        case 3 :
+        System.out.println("Insuffisant");
+        break;
+        case 4 :
+        System.out.println("Suffisant");
+        break;
+        case 5 :
+        System.out.println("Bien");
+        break; 
+        case 6 :
+
+
+
+
+        default:
+            break;
+    }
     }
 
   
--- exercices/exercice06/ExerciceSwitch2.java
+++ exercices/exercice06/ExerciceSwitch2.java
@@ -2,7 +2,9 @@ package exercice06;
 
 public class ExerciceSwitch2 { public static void main(String[] args) {
     
-    
+    int age = 7;
+
+    if 
 
 }
     
