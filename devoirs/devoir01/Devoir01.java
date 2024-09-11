0788212091c3338e1b514dcbe0b08bd92fe5e251
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-11 15:13:44 +0200

    devoir

---
+++ devoirs/devoir01/Devoir01.java
@@ -0,0 +1,20 @@
+package devoirs.devoir01;
+
+public class devoir01 {
+    public static void main(String[] args) {
+
+        int monAge = 22;
+        int estMajeur;
+
+        if monAge > 20 ;
+        (estMajeur= true)
+
+    System.out.println("Je m'appelle Wa Mbengi Loïc");
+    System.out.println("Mon âge est de:" + monAge );
+
+    if estMajeur = true ;
+(System.out.println("Je suis majeur"));
+
+    }
+
+}
