8134ad41f1ff40fb9d53b1484c3774c04c9d7d0d
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-12 14:51:04 +0200

    structure base devoir

--- devoirs/devoir02/Devoir02.java
+++ devoirs/devoir02/Devoir02.java
@@ -3,21 +3,31 @@ package devoirs.devoir02;
 public class Devoir02 {
     public final static int CAPACITE_RESERVOIR_A = 3;
     public final static int CAPACITE_RESERVOIR_B = 5;
-
+// à quoi sert les constantes ?
     public static void main(String[] args) {
 
         int reservoirA = 0;
         int reservoirB = 0;
 
-        int MIN = 30;
-        int MAX = 120;
+        int MIN = 2;
+        int MAX = 4;
 
         int remplissage = 8;
 
         int nombre = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
 
-        System.out.println(remplissage);
-        // reprendre ligne 13
+        System.out.println("Valeur de remplissage " + nombre);
+
+
+        while (reservoirA + reservoirB < remplissage) { 
+            reservoirA++; remplissage--; 
+            System.out.println(reservoirA + reservoirB);
+            if (reservoirA == 3) { 
+                reservoirB++ ; remplissage--;
+                
+            } 
+
+        }
     }
 
 }
