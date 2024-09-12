e697450ea04094b2183d760d582c62c44fd7af2a
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-12 08:33:03 +0200

    devoir correction

--- devoirs/devoir01/Devoir01.java
+++ devoirs/devoir01/Devoir01.java
@@ -1,19 +1,30 @@
 package devoirs.devoir01;
 
-public class devoir01 {
+public class Devoir01 {
     public static void main(String[] args) {
 
         int monAge = 22;
-        int estMajeur;
+     
+        boolean estMajeur ;
+      
+        System.out.println("Je m'appelle Wa Mbengi Loïc");
+        System.out.println("Mon âge est de " + monAge + " ans" );
 
-        if monAge > 20 ;
-        (estMajeur= true)
+        if (monAge > 20) {
+            estMajeur = true;
+        } else {
+            estMajeur = false;
+        }
 
-    System.out.println("Je m'appelle Wa Mbengi Loïc");
-    System.out.println("Mon âge est de:" + monAge );
+        if (estMajeur == true) { System.out.println( "Loïc est Majeur");
+            
+        }
 
-    if estMajeur = true ;
-(System.out.println("Je suis majeur"));
+        if (estMajeur ==false) { System.out.println("Loïc n'est pas majeur");}
+
+        
+  
+    
 
     }
 
