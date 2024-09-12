82ab2173259f20149b90bd715e7d479632b5aedd
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-12 13:51:49 +0200

    début devoir

---
+++ devoirs/devoir02/Devoir02.java
@@ -0,0 +1,15 @@
+package devoirs.devoir02;
+
+public class Devoir02 {
+    public final static int CAPACITE_RESERVOIR_A = 3;
+    public final static int CAPACITE_RESERVOIR_B = 5;
+
+    public static void main(String[] args) {
+
+        int reservoirA = 0 ;
+        int reservoirB = 0 ;
+    
+        int remplissage;
+    }
+
+}
