d5080c02b64642b747a201ea1d513b5e75ea9ab8
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-17 16:22:41 +0200

    devoir

--- devoirs/devoir02/Devoir02.java
+++ devoirs/devoir02/Devoir02.java
@@ -3,30 +3,38 @@ package devoirs.devoir02;
 public class Devoir02 {
     public final static int CAPACITE_RESERVOIR_A = 3;
     public final static int CAPACITE_RESERVOIR_B = 5;
-// à quoi sert les constantes ?
+
+    // à quoi sert les constantes ?
     public static void main(String[] args) {
 
         int reservoirA = 0;
         int reservoirB = 0;
 
-        int MIN = 2;
-        int MAX = 4;
+        int MIN = 0;
+        int MAX = 8;
 
-        int remplissage = 8;
+        int remplissage = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
 
-        int nombre = (int) (Math.random() * (MAX - MIN + 1)) + MIN;
+        System.out.println("Valeur de remplissage " + remplissage);
 
-        System.out.println("Valeur de remplissage " + nombre);
+        // j'ai du mal à appliquer la consigne plus loin que çaje ne comprends pas ce
+        // qui se passe dans mon programme j'ai essayé de demander à chatgpt de m'expliquer ce qzui m'a permis d^'apporter deux trois amélioration mais je reste bloqué ici
 
+        while (reservoirA + reservoirB < remplissage) {
+            reservoirA++;
+            remplissage--;
+            System.out.println("reservoir a : " + reservoirA + " / reservoir b : "+ reservoirB);
+            System.out.println("reservoir de remplissage " + remplissage);
+            if (remplissage > 0) {
+                break;
 
-        while (reservoirA + reservoirB < remplissage) { 
-            reservoirA++; remplissage--; 
-            System.out.println("reservoir a et b : " + reservoirA + reservoirB);
-            System.out.println("reservoir de remplissage "+remplissage );
-            if (reservoirA == 3) { 
-                reservoirB++ ; remplissage--;
-                
-            } 
+            }
+            else if (reservoirA == 3) {
+                reservoirB++;
+                remplissage--;
+
+            }
+           
 
         }
     }
