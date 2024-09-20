7ba7835166fd48c6cb177278db5f063a285a6738
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-09-20 13:36:40 +0200

    3

---
+++ devoirs/devoir03/Devoir03.java
@@ -0,0 +1,13 @@
+package devoirs.devoir03;
+
+public class Devoir03 { public static void main (String[]args){
+    int mois = 9;
+    int annee = 2024;
+    int jour = 19;
+
+    // déclare un tableau avec les 365 jours de l'année
+    // un tableau pour les mois
+    // un tableau des années entre 0 et 9999
+}
+    
+}
