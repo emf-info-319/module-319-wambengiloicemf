b5091a3cc0a7e847ecb57bdbad33fde4d2cc98ba
wambengiloicemf <WaMbengiL@studentfr.ch>
2024-11-07 09:04:49 +0100

    new

--- exercices/exercice12/CodageRound.java
+++ exercices/exercice12/CodageRound.java
@@ -2,10 +2,39 @@ public class CodageRound {
 
     public final static double PI = 3.14;
 
-
     public static void main(String[] args) {
+
+        int[] rayons = new int[10];
+
+        int min = 1;
+        int max = 10;
+
+        for (int index = 0; index < rayons.length; index++) {
+            rayons[index] = genereNombre(min, max);
+
+
+        }
+
         
-        int[] tableau = new int[10];
+
+
+
+        for (int i = rayons.length - 1; i >= 0; i--) {
+            if (rayons [i] > 50) {
+              
+            }
+       
+
+        }
+
+
     }
-    
+
+    public static int genereNombre(int min, int max) {
+
+        int random = (int) (Math.random() * (max - min + 1)) + min;
+        return random;
+
+    }
+
 }
